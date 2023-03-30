1de379c92234a4c1d4a88a778e67358f3f43d2b3
stephMerlu <stephanie.merluzzo@gmail.com>
2023-03-30 15:50:32 +0200

    affichage page presentation pour chaque shoe

--- src/Controller/PresentationShoeController.php
+++ src/Controller/PresentationShoeController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Shoes;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PresentationShoeController extends AbstractController
 {
-    #[Route('/presentation/shoe', name: 'app_presentation_shoe')]
-    public function index(): Response
+    #[Route('/{id}/shoes', name: 'app_presentation_shoe')]
+    public function index(Shoes $shoes): Response
     {
         return $this->render('presentation_shoe/index.html.twig', [
             'controller_name' => 'PresentationShoeController',
+            'shoe' => $shoes
         ]);
     }
 }
